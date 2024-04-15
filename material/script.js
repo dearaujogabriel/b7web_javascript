70d0c0e8dfd9270a13cc81e270a459c0a28b190b
Gabriel de Araujo <gabrielaraujovni@gmail.com>
2024-04-15 14:41:25 -0300

    funcoes, condicao ternaria, switch

---
+++ material/script.js
@@ -0,0 +1,59 @@
+/*
+let x = 10
+let y = 5
+console.log(x > y)
+
+let w = "10"
+let z = 10
+console.log(w === z)
+
+let a = 10
+let b = 5
+console.log(a > b)
+
+let preco = 40.3
+if (preco <= 45) {
+    console.log("Está barato")
+} else {
+    console.log("Está caro")
+}
+
+
+$$$$$$$$$$$$$$$$$$$ condicional ternaria $$$$$$$$$$$$$$$$$$$
+let isMember = false
+let shipping = isMember ? 2 : 10
+
+console.log(isMember ? "Voce é membro" : "Voce nao é membro")
+console.log("Frete: " + shipping)
+
+
+let age = 59
+let isAdult = (age >= 18 && age < 60 ? "Sim" : "Nao")
+console.log(isAdult)
+
+
+$$$$$$$$$$$$$$$$$$$ Switch $$$$$$$$$$$$$$$$$$$
+
+
+let profissao = "QA"
+console.log("Sua profissao: " + profissao)
+
+switch (profissao) {
+    case "Programador":
+        console.log("Cor da camisa: Verde")
+        break
+    case "Policial":
+        console.log("Cor da camisa: Preta")
+        break
+    case "Bombeiro":
+        console.log("Cor da camisa: Vermelha")
+        break
+    case "Fiscal":
+        console.log("Cor da camisa: Branca")
+        break
+    default:
+        console.log("Cor da camisa: Azul")
+        break
+}
+
+*/
--- script.js
+++ script.js
@@ -1,1 +1,78 @@
-console.log("Ola, mundo!")
+function gravidade() {
+    console.log("A gravidade da Terra é: ")
+    console.log(9.8)
+}
+//gravidade()
+
+
+function somar(x, y) {
+    let resultado = x + y
+    console.log("Resultado: " + resultado)
+}
+
+//somar(10, 20)
+
+function nomeCompleto(nome, sobrenome) {
+    let completo = `${nome} ${sobrenome}`
+    return completo
+}
+
+//console.log(nomeCompleto("Gabriel", "Araujo"))
+//console.log(nomeCompleto("Ana Valeria", "Azevedo"))
+//console.log(nomeCompleto("Carlos Eduardo", "Azevedo Araujo"))
+
+
+function menorDeIdade(idade) {
+    if (idade >= 18) {
+        return true
+    } else {
+        return false
+    }
+}
+
+let verificacao = (menorDeIdade(17))
+if (verificacao) {
+    //console.log("Maior de idade")
+} else {
+    //console.log("Menor de idade")
+}
+
+function maiorDeIdade(idade) {
+    let verificacao = idade >= 18 ? "Maior de idade" : "Menor de idade"
+    return verificacao
+}
+//console.log(maiorDeIdade(19))
+
+function calcPct(n1, n2) {
+    return n2 / n1 * 100
+}
+let x = 40
+let y = 10
+let pct = calcPct(x, y)
+//console.log(`${pct}% de ${x} é ${y}`)
+
+function calcularImovel(metragem, quartos) {
+    let m2 = 30000
+    let preco = 0
+    switch (quartos) {
+        case 1:
+            preco = metragem * m2
+            break
+        case 2:
+            preco = metragem * (m2 * 1.2)
+            break
+        case 3:
+            preco = metragem * (m2 * 1.5)
+            break
+        default:
+            preco = metragem * (m2 * 2)
+            break
+    }
+    return preco
+}
+
+let metragem = 123
+let quartos = 2
+console.log(`A casa custa R$ ${calcularImovel(metragem, quartos)}`)
+
+
