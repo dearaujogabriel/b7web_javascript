d7e84d7aef2d660aaf715f6c2ea0fa94ce8cd6f5
Gabriel de Araujo <gabrielaraujovni@gmail.com>
2024-04-21 22:29:14 -0300

    Aula 12

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-Teste
+// function square(x) {
+//     return x * x
+// }
+
+// function addSquares(a, b) {
+//     return square(a) + square(b)
+// }
+
+function addSquares(a, b) {
+    const square = (x) => x * x
+    return square(a) + square(b)
+}
+
+
+console.log(addSquares(2, 3))
+
