78cb7ea02498cfd77cb485ce921a7505239d2df2
Gabriel de Araujo <gabrielaraujovni@gmail.com>
2024-04-23 20:15:22 -0300

    Aula 17, modulo 2

--- script.js
+++ script.js
@@ -13,3 +13,26 @@ let personagem = {
 
 console.log(`${personagem.nome} tem ${personagem.idade} anos e mora no ${personagem.pais}. Tem ${personagem.caracteristicas.forca} pontos de forca, ${personagem.caracteristicas.magia} pontos de magia e sua classe eh ${personagem.caracteristicas.classe}`)
 console.log(`Seu olho esquerdo eh ${personagem.olhos[0]} e seu olho direito eh ${personagem.olhos[1]}`)
+
+personagem.nome = 'Cadu'
+
+console.log(`${personagem.nome}`)
+
+personagem.caracteristicas.forca += 5
+
+console.log(`${personagem.caracteristicas.forca}`)
+
+personagem.olhos.push('Verde')
+console.log(personagem.olhos[2])
+
+let persona = {
+    nome: 'Gabriel',
+    idade: 26, 
+    carros: [
+        {modelo: 'Ferrari', cor: 'Vermelho'},
+        {modelo: 'Volvo', cor: 'Preto'},
+        {modelo: 'Fiat', cor: 'Verde'}
+    ]
+}
+console.log(persona.carros[0].modelo)
+console.log(persona.carros[0].cor)
