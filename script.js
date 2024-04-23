9381fd32dc7b79f73afb790767efebea27ebfd6e
Gabriel de Araujo <gabrielaraujovni@gmail.com>
2024-04-23 20:43:56 -0300

    Aula 18, modulo 2

--- script.js
+++ script.js
@@ -1,38 +1,22 @@
-let personagem = {
+let pessoa = {
     nome: 'Gabriel',
+    sobrenome: 'Araujo', 
     idade: 26,
-    pais: 'Brasil',
-    sexo: 'Masculino',
-    caracteristicas: {
-        forca: 100,
-        magia: 100,
-        classe: 'Nobre'
-    },
-    olhos: ['Azul', 'Vermelho']
+    nomeCompleto: function () {
+        return this.nome + ' ' + this.sobrenome
+    }
 }
 
-console.log(`${personagem.nome} tem ${personagem.idade} anos e mora no ${personagem.pais}. Tem ${personagem.caracteristicas.forca} pontos de forca, ${personagem.caracteristicas.magia} pontos de magia e sua classe eh ${personagem.caracteristicas.classe}`)
-console.log(`Seu olho esquerdo eh ${personagem.olhos[0]} e seu olho direito eh ${personagem.olhos[1]}`)
+console.log(pessoa.nomeCompleto())
 
-personagem.nome = 'Cadu'
-
-console.log(`${personagem.nome}`)
-
-personagem.caracteristicas.forca += 5
-
-console.log(`${personagem.caracteristicas.forca}`)
-
-personagem.olhos.push('Verde')
-console.log(personagem.olhos[2])
-
-let persona = {
+// em funcoes anonimas nao funciona pois uma funcao sem pai nao pode acessar os atributos do proprio objeto
+let pessoas = {
     nome: 'Gabriel',
-    idade: 26, 
-    carros: [
-        {modelo: 'Ferrari', cor: 'Vermelho'},
-        {modelo: 'Volvo', cor: 'Preto'},
-        {modelo: 'Fiat', cor: 'Verde'}
-    ]
+    sobrenome: 'Araujo', 
+    idade: 26,
+    nomeCompleto: () => {
+        return this.nome + ' ' + this.sobrenome
+    }
 }
-console.log(persona.carros[0].modelo)
-console.log(persona.carros[0].cor)
+
+console.log(pessoas.nomeCompleto())
