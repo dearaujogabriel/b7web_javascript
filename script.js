41b54bf2fb32854e513714c8cf88a079bd1acdcb
Gabriel de Araujo <gabrielaraujovni@gmail.com>
2024-04-24 20:55:28 -0300

    Aula 22, modulo 2

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-let numero = 0
-
-while(numero < 10){
-    console.log('While ' + numero)
-    numero++
+let fruits = ['Maca', 'Uva', 'Banana']
+for (let i in fruits) {
+    console.log(fruits[i])
 }
 
-for(let numero = 0; numero < 10; numero++) {
-    console.log('For ' + numero)
+console.log('===== Outro metodo =====')
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i])
+
+}
+console.log('===== Outro metodo =====')
+for (fruit of fruits) {
+    console.log(fruit)
+}
+
+
+let i = 1
+while (i <= 100) {
+    console.log(i)
+    i++
 }
