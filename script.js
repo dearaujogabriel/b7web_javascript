7107bbc72ec8b8345e0d7c153e8ef5507fd9c077
Gabriel de Araujo <gabrielaraujovni@gmail.com>
2024-04-27 21:10:41 -0300

    Aula 4, modulo 3

--- script.js
+++ script.js
@@ -1,39 +1,17 @@
-let fruits = ['Ameixa', 'Maca', 'Laranja', 'Banana', 'Uva']
+/*
 
-let bigFruits = fruits.filter((item) => {
-    if (item.length > 4) {
-        return true
-    } else {
-        return false
-    }
-})
-
-console.log(bigFruits)
-console.log(fruits)
+DOM = Document Object Model
+Cada elemento presente no documento de HMTL e um elemento do DOM
+DOM basicamente eh uma organizacao de como a pagina vai ser exibida
 
 
-let ok = fruits.every((value) => { 
-    return value.length > 3 // retorna true se todos os itens do array satisfazem a condicao
-})
+*/
 
-if(ok){
-    console.log('Todos sao maiores que 3')
-} else {
-    console.log('Nao sao todos que sao maiores que 3')
+function clicou() {
+    alert('Clicou no botao')
 }
 
-let vasco = fruits.some((value) => { 
-    return value.length > 20 // retorna true se algum dos itens do array satisfaz a condicao
+let botao = document.querySelector('.botao')
+botao.addEventListener("click", () => {
+    clicou()
 })
-
-if(vasco){
-    console.log('Algum dos itens sao maiores que 20')
-} else {
-    console.log('Nenhum item e maior que 20')
-}
-
-if (fruits.includes('Uva')) { // verifica se contem o item especificado no array
-    console.log('Tem uva')
-} else {
-    console.log('Nao tem uva')
-}
