022086f87cd6dc521352fdcfdfde903774fd94df
Gabriel Araujo <gabrielaraujovni@gmail.com>
2024-05-05 22:55:48 -0300

    Aula 12, modulo 3

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-
-// manipular eventos de teclado
-
-function apertou() {
-    console.log('Apertou')
-}
-function segurou() {
-    console.log('Segurou')
-}
-function soltou() {
-    console.log('Soltou')
+function soltou(e) {
+    //console.log(e.key) // mostra de maneira mais simples qual tecla foi pressionada
+    console.log('Tecla apertada ' + e.code) // mais completo, mostra o nome mais detalhado da tecla de input
+    console.log('Shift? ' + e.shiftKey) // retorna true ou false se a tecla estava apertada
+    console.log('Control? ' + e.ctrlKey)
+    console.log('Alt? ' + e.altKey)
+    console.log('------------') 
 }
 
-document.addEventListener('keyup', soltou)
+const input = document.querySelector('input')
+input.addEventListener('keyup', soltou)
