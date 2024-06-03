d66a555e427220f145435011d337b7c1e1a30aa0
Gabriel Araujo <gabrielaraujovni@gmail.com>
2024-06-03 13:10:12 -0300

    Static

--- poo/script.js
+++ poo/script.js
@@ -1,33 +1,13 @@
 class Person {
     
+    static hands = 2
     age = 0
 
-    constructor(firstName) {
-        this.firstName = firstName
+    constructor(name) {
+        this.name = name
     }
-
-    sayHi() {
-        console.log(`${this.firstName} diz 'Oi'`)
-    }
-
 }
 
-class Student extends Person {
+let p1 = new Person("Gabriel")
 
-    constructor(name, id) {
-        super(name)
-        this.id = id
-    }
-
-    sayHello() {
-        super.sayHi()
-    }
-
-}
-
-let p1 = new Student('Gabriel', 1)
-p1.age = 20
-
-p1.sayHello()
-
-console.log(`${p1.firstName} tem ${p1.age} e matricula ${p1.id}`)
+console.log(`Me chamo ${p1.name} e tenho ${Person.hands} maos`)
