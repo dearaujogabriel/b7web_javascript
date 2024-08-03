fec3918a657f2e478b5d65c731845065adf4d3a0
Gabriel Araujo <gabrielaraujovni@gmail.com>
2024-08-03 16:58:35 -0300

    heranca

--- programacao_funcional/script.js
+++ programacao_funcional/script.js
@@ -1,21 +1,14 @@
-function createPerson(name, lastname, age) {
-    return {
-        name,
-        lastname,
-        age,
-        getFullName() { return `${this.name} ${this.lastname}` },
-        start() { return `Deu start na pessoa` }
-    }
+const defaultUser = {
+    name: '',
+    email: '',
+    permissionLvl: 1
 }
 
+const user1 = {
+    ...defaultUser,
+    name: 'Gabriel',
+    email: 'gabriel@vasco.com',
+    permissionLvl: 3
+}
 
-let person1 = createPerson('Gabriel', 'Araujo', 26)
-let person2 = createPerson('Ana', 'Azevedo', 21)
-let person3 = createPerson('Cadu', 'Azevdo Araujo', 2)
-
-console.log(`${person1.name}       ${person2.name}       ${person3.name}`)
-console.log(`${person1.lastname}   ${person2.lastname}   ${person3.lastname}`)
-console.log(`${person1.age}        ${person2.age}        ${person3.age}`)
-
-console.log(person1.start())
-console.log(person1.getFullName())
+console.log(user1)
